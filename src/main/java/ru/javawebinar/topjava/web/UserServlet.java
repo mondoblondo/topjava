3813be5f97c94a56ddc1645971ae7ee0d1c34a7d
Artes <AlexTingaev@uopeople.edu>
2017-03-31 16:08:39 +0300

    HW_1 add empty MEAL JSP and mapping for him

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.slf4j.LoggerFactory.getLogger;
+
 
 
 /**
@@ -16,7 +17,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * Date: 19.08.2014
  */
 public class UserServlet extends HttpServlet {
-    private static final Logger LOG = getLogger(UserServlet.class);
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         LOG.debug("redirect to users");
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: artes
+  Date: 31.03.17
+  Time: 14:12
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meal List</title>
+</head>
+<body>
+
+<h2><a href="index.html">Home</a></h2>
+<h2>Meal list</h2>
+
+<h3>Здесь</h3>
+<h3>будет</h3>
+<h3>что-то полезное</h3>
+
+</body>
+</html>
